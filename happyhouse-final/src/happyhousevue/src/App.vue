398c023d80844b9006a9abede92051759a0ed071
CHKIM <CHKIM@LAPTOP-LPAM3ARV>
2020-11-18 17:43:30 +0900

    [revise] connect git and vue

--- happyhouse-final/src/happyhousevue/src/App.vue
+++ happyhouse-final/src/happyhousevue/src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <h1>Hello World!!!!!</h1>
     </div>
     <router-view />
   </div>
