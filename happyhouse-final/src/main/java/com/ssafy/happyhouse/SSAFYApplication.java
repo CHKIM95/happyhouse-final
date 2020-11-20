049cf6eb29eaa5077d121b33f7ba093859ba68a0
multicampus <multicampus@M805>
2020-11-20 14:33:15 +0900

    [revise] qna.xml [create] swagger function

---
+++ happyhouse-final/src/main/java/com/ssafy/happyhouse/SSAFYApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.happyhouse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@SpringBootApplication
+@EnableTransactionManagement
+public class SSAFYApplication {
+    // 이것을 실행시키면 레스트풀 메인 시작
+	public static void main(String[] args) {
+		SpringApplication.run(SSAFYApplication.class, args);
+	}
+}
+//http://localhost:8197/ssafyvue/swagger-ui.html
+//http://localhost:8297/ssafyvue/swagger-ui.html
